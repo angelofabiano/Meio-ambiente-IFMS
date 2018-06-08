577d5d4924a65734d5508f6ca12e2b5732de3d57
laboratorio <laboratorio@LaboratorioA09.labifms.edu.br>
2018-06-07 21:20:41 -0400

    primeiro commint

---
+++ src/meio/ambiente/ifms/MeioAmbienteIFMS.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package meio.ambiente.ifms;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class MeioAmbienteIFMS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("meio.ambiente.ifms.MeioAmbienteIFMS.main()");
+        System.out.println("hello mundo");
+    }
+    
+}
